e88d47b872277e60cf693b115d5b9b4bb7e75e91
adamyaxley <craincat666@gmail.com>
2017-12-05 08:06:50 +0900

    Pass standardese binary location to generateDocs.py via --standardese-path

--- Script/generateDocs.py
+++ Script/generateDocs.py
@@ -1,11 +1,16 @@
 import os
 import shutil
+import argparse
 import common
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--standardese-path", dest="standardesePath", action="store", help="The path to the standardese binary", required=True)
+    args = parser.parse_args()
+    
     buildDir = os.path.join(common.getRoot(), "_Temp", "Docs", "Engine")
     common.mkdir(buildDir)
-    common.callBlockingWithOutput(["cmake", "-DGenerateDocs=ON", "-B" + buildDir, "-H" + common.getRoot()])
+    common.callBlockingWithOutput(["cmake", "-DGenerateDocs=ON", "-B" + buildDir, "-H" + common.getRoot(), "-DSTANDARDESE_TOOL=" + args.standardesePath])
     common.callBlockingWithOutput(["cmake", "--build", buildDir, "--target", "standardese_docs_engine"])
     
     # Copy generated docs to Docs
