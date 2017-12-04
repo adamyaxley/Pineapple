554be0acabd85cdb80a23f0d9e40dae030f91c26
adamyaxley <craincat666@gmail.com>
2017-12-05 07:43:25 +0900

    Copy generated doc files to Docs/

--- Script/generateDocs.py
+++ Script/generateDocs.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import common
 
 def main():
@@ -6,6 +7,19 @@ def main():
     common.mkdir(buildDir)
     common.callBlockingWithOutput(["cmake", "-DGenerateDocs=ON", "-B" + buildDir, "-H" + common.getRoot()])
     common.callBlockingWithOutput(["cmake", "--build", buildDir, "--target", "standardese_docs_engine"])
+    
+    # Copy generated docs to Docs
+    generatedDir = os.path.join(buildDir, "standardese_docs_engine")
+    copyDir = os.path.join(common.getRoot(), "Docs", "Engine")
+    print("Copying generated docs from {} to {}".format(generatedDir, copyDir))
+    common.mkdir(copyDir)
+    docFiles = common.getFiles(generatedDir, ".md")
+    print(docFiles)
+    for file in docFiles:
+        print("Copying {} to {}".format(file, copyDir))
+        shutil.copy2(file, copyDir)
+    
+    print("Done!")
     
 if __name__ == "__main__":
     main()
