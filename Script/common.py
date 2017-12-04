632c0552a0b9cab551d9f306bce1c1f4b608b3f0
adamyaxley <craincat666@gmail.com>
2017-12-05 08:08:58 +0900

    Tabs to spaces in python scripts

--- Script/common.py
+++ Script/common.py
@@ -3,16 +3,16 @@ import errno
 import subprocess
 
 def getFiles(dir = ".", exts = ""):
-	files = []
-	
-	for dirname, dirnames, filenames in os.walk(dir):
-		for filename in filenames:
-			for ext in exts.split(";"):
-				if filename.endswith(ext):
-					files.append(os.path.join(dirname, filename))
-					break
-	
-	return files
+    files = []
+    
+    for dirname, dirnames, filenames in os.walk(dir):
+        for filename in filenames:
+            for ext in exts.split(";"):
+                if filename.endswith(ext):
+                    files.append(os.path.join(dirname, filename))
+                    break
+    
+    return files
 
 def getRoot():
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
